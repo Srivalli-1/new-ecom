3d0ff94ef770985292d253ff2c6740b4a95354c7
Srivalli-1 <kajasrivalli5@gmail.com>
2025-03-20 09:58:41 +0530

    done

---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import SignupPage from '../src/components/Signup/signup'
+const Signup = () => {
+  return (
+    <div>
+      <SignupPage/>
+    </div>
+  )
+}
+
+export default Signup
